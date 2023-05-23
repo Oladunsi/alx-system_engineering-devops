1da41b4c0b2019b81e2123ea9c9cb529bdd859f0
Oladunsi <okeoladunsi6@gmail.com>
2023-05-23 17:26:48 +0000

    requirement fix by removing trailling whitespace from export to json file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,7 +10,7 @@
 
    Records all tasks that are owned by this employee
    Format must be: { "USER_ID":
-                     [{"task": "TASK_TITLE", 
+                     [{"task": "TASK_TITLE",
                        "completed": TASK_COMPLETED_STATUS,
                        "username": "USERNAME"},
                      {"task": "TASK_TITLE",
